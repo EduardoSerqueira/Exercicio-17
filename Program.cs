b1609181067334155e4d09437850aec006372eb6
e.serqueira <eduardo.serqueira@outlook.com>
2020-05-20 17:02:41 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercicio_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("----FOR-----");
+
+            for (int i = 0; i <= n; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.WriteLine("----WHILE-----");
+
+            int w = 0;
+
+            while (w <= n)
+            {
+                if (w % 2 != 0)
+                {
+                    Console.WriteLine(w);
+                }
+                w++;
+            }
+        }
+    }
+}
